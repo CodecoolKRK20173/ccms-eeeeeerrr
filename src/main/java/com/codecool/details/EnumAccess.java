ae81ed3b67352c087fa16a2e2adf158a14e75cb3
Elzacodecool <elliegolec@gmail.com>
2018-05-15 15:46:15 +0200

    Add accesses and getPrivileges method

--- src/main/java/com/codecool/details/EnumAccess.java
+++ src/main/java/com/codecool/details/EnumAccess.java
@@ -1,4 +1,35 @@
 package com.codecool.details;
 
+import java.util.List;
+
 public enum EnumAccess {
+    STUDENT {
+        @Override
+        public List<EnumPrivileges> getPrivileges() {
+
+            return null;
+        }
+    },
+    MANAGER {
+        @Override
+        public List<EnumPrivileges> getPrivileges() {
+
+            return null;
+        }
+    },
+    EMPLOYEE {
+        @Override
+        public List<EnumPrivileges> getPrivileges() {
+
+            return null;
+        }
+    },
+    MENTOR {
+        @Override
+        public List<EnumPrivileges> getPrivileges() {
+
+            return null;
+        }
+    };
+    public abstract List<EnumPrivileges> getPrivileges();
 }
