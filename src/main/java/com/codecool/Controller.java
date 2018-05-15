d26885551914531a7dd5003dba1e998750ea318f
Elzacodecool <elliegolec@gmail.com>
2018-05-15 15:59:22 +0200

    Add signIn and handleMenu method

--- src/main/java/com/codecool/Controller.java
+++ src/main/java/com/codecool/Controller.java
@@ -1,4 +1,22 @@
 package com.codecool;
 
+import com.codecool.DAO.CodecoolDAOEmployee;
+import com.codecool.DAO.CodecoolDAOStudent;
+import com.codecool.person.CodecoolPerson;
+
+import java.util.List;
+
 public class Controller {
+    private CodecoolDAOStudent csvDAOEmployee;
+    private CodecoolDAOEmployee csvDAOStudent;
+    private CodecoolPerson user;
+    private List<String> assignmentList;
+
+    public void signIn() {
+
+    }
+    
+    public void handleMenu() {
+
+    }
 }
