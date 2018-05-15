3264e2302ae6ee0c3c40e688243b68611141ee7a
rafalkordas <rafalkordas063@gmail.com>
2018-05-15 22:11:18 +0200

    Implement DAOInterfaceStudent

--- src/main/java/com/codecool/DAO/DAOInterfaceStudent.java
+++ src/main/java/com/codecool/DAO/DAOInterfaceStudent.java
@@ -1,4 +1,22 @@
 package com.codecool.DAO;
 
+import com.codecool.details.Assignment;
+import com.codecool.person.Student;
+
+import java.util.List;
+
 public interface DAOInterfaceStudent {
+
+    AccessLevelList ACCESS;
+    void addAssignment(Assignment assignment);
+    void gradeAssignment(Student student, Assignment assignment, int grade);
+    void checkAttendence(Student student);
+    void addStudent(Student student);
+    void removeStudent(Student student);
+    void editStudent(Student student);
+    void submitAssignment(Student student, Assignment assignment);
+    List<Assignment> getAllGrades(Student student);
+    List<Student> getAllStudent();
+
 }
+
