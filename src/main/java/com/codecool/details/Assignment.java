68221096d89a8eda7a88cffaf5afcdad2e3095a6
rafalkordas <rafalkordas063@gmail.com>
2018-05-16 11:40:16 +0200

    Add and implement class Assignment

--- src/main/java/com/codecool/details/Assignment.java
+++ src/main/java/com/codecool/details/Assignment.java
@@ -1,4 +1,32 @@
 package com.codecool.details;
 
 public class Assignment {
+    private final String NAME;
+    private boolean isSubmitted;
+    private int grade;
+
+    public Assignment(String name){
+        this.NAME = name;
+        this.isSubmitted = false;
+    }
+
+    public int getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setSubmitted() {
+        this.isSubmitted = true;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public boolean getIsSubmitted() {
+        return this.isSubmitted;
+    }
 }
