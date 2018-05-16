0109545a864342e843e860024d87ba35985bed0f
rafalkordas <rafalkordas063@gmail.com>
2018-05-16 11:12:35 +0200

    Add constructor and import

--- src/main/java/com/codecool/DAO/CodecoolDAOStudent.java
+++ src/main/java/com/codecool/DAO/CodecoolDAOStudent.java
@@ -1,4 +1,20 @@
 package com.codecool.DAO;
 
-public class CodecoolDAOStudent {
-}
+import com.codecool.details.Assignment;
+import com.codecool.person.Student;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodecoolDAOStudent implements DAOInterfaceStudent{
+
+    private List<Student> studentList;
+    private String file = "student.csv";
+
+    public CodecoolDAOStudent() {
+        this.studentList = new ArrayList<Student>();
+        readFile(file);
+    }
+
+    
