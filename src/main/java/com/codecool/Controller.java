a11d289888747c2536e3d4d1d179224583917951
Elzacodecool <elliegolec@gmail.com>
2018-05-16 11:07:19 +0200

    Implement signUp method

--- src/main/java/com/codecool/Controller.java
+++ src/main/java/com/codecool/Controller.java
@@ -7,13 +7,42 @@ import com.codecool.person.CodecoolPerson;
 import java.util.List;
 
 public class Controller {
-    private CodecoolDAOStudent csvDAOEmployee;
-    private CodecoolDAOEmployee csvDAOStudent;
+    private CodecoolDAOStudent csvDAOStudent;
+    private CodecoolDAOEmployee csvDAOEmployee;
     private CodecoolPerson user;
     private List<String> assignmentList;
+    private View view = new View();
 
     public void signIn() {
+        String login = view.askUserLogin();
+        String password = view.askUserPassword();
 
+        user = searchStudent(login, password);
+        if (user == null) {
+            user = searchEmployee(login, password);
+        }
+    }
+
+    private CodecoolPerson searchStudent(String login, String password) {
+        csvDAOStudent = new CodecoolDAOStudent();
+
+        for (CodecoolPerson student : csvDAOStudent.getAllStudents()) {
+            if (student.getLogin().equals(login) && student.getPassword().equals(password)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    private CodecoolPerson searchEmployee(String login, String password) {
+        csvDAOEmployee = new CodecoolDAOEmployee();
+
+        for (CodecoolPerson employee : csvDAOEmployee.getAllEmployee()) {
+            if (employee.getLogin().equals(login) && employee.getPassword().equals(password)) {
+                return employee;
+            }
+        }
+        return null;
     }
     
     public void handleMenu() {
