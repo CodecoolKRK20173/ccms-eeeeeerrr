7b9515debc6547ed26a273d4584e11ef30d65a86
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-05-16 14:46:06 +0200

    Implement Constructor

--- src/main/java/com/codecool/person/Manager.java
+++ src/main/java/com/codecool/person/Manager.java
@@ -1,4 +1,9 @@
 package com.codecool.person;
 
 public class Manager extends CodecoolPerson{
+
+    public Manager(String name, String surName, String login, String password) {
+        super(name, surName, login, password);
+        this.accessLevel = Access.MANAGER;
+    }
 }
