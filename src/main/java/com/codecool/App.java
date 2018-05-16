8015c044746ab104d2d3659bf6b3a9e4d5c14e46
Elzacodecool <elliegolec@gmail.com>
2018-05-16 12:08:36 +0200

    Test enums

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,7 +1,11 @@
 package com.codecool;
 
+import com.codecool.details.EnumPrivileges;
+
 public class App {
     public static void main(String[] args) {
+        EnumPrivileges enumPrivileges = EnumPrivileges.ADD_ASSIGMENT;
+        System.out.println(enumPrivileges.toString());
 
     }
 }
