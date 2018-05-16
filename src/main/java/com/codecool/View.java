b22b16e9bb9d547414a4797714cb275615a2fdd4
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-05-17 01:31:25 +0200

    Implement askUserPassword, displayLine, clearScreen.

--- src/main/java/com/codecool/View.java
+++ src/main/java/com/codecool/View.java
@@ -3,13 +3,16 @@ package com.codecool;
 import com.codecool.details.Access;
 import com.codecool.details.Privilege;
 
+import java.io.Console;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class View {
     Scanner scanner = new Scanner(System.in);
+    Console cnsl;
     public void displayMenu(List<Privilege> privileges) {
-        System.out.println("What would you like to do:");
+        displayLine("What would you like to do:");
         for (int i = 0; i < privileges.size(); i++) {
             System.out.printf("\t(%d) %s\n", i, privileges.get(i).toString());
         }
@@ -20,9 +23,34 @@ public class View {
     }
 
     public String askUser(String message) {
-        System.out.println(message);
+        displayLine(message);
         return scanner.nextLine();
     }
 
+    public String askUserPassword() {
+        cnsl = System.console();
+        String password;
+        char[] asterix;
 
+        if(cnsl !=  null) {
+            char[] passwordArray = cnsl.readPassword("Provide password: ");
+            asterix = new char[passwordArray.length];
+            Arrays.fill(asterix, '*');
+            displayLine(String.valueOf(asterix));
+            password = new String(passwordArray);
+
+        } else {
+            password = askUser("Provide password: ");
+        }
+        return password;
+
+    }
+
+    public void displayLine(String lineContent) {
+        System.out.println(lineContent);
+    }
+
+    public void clearScreen() {
+        displayLine("\033[H\033[2J");
+    }
 }
