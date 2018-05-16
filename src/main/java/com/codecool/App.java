0f7d6a9b6dd972035dd75cce7679865aef54f96b
Elzacodecool <elliegolec@gmail.com>
2018-05-16 14:25:22 +0200

    Test

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,11 +1,22 @@
 package com.codecool;
 
-import com.codecool.details.EnumPrivileges;
+import com.codecool.details.Access;
+import com.codecool.details.Privilege;
 
 public class App {
     public static void main(String[] args) {
-        EnumPrivileges enumPrivileges = EnumPrivileges.ADD_ASSIGMENT;
-        System.out.println(enumPrivileges.toString());
+        Access enumAccess = Access.MANAGER;
+        for (Privilege enumPrivileges : enumAccess.getPrivileges()) {
+            System.out.println(enumPrivileges.toString());
+        }
 
+        Access enumAccess2 = Access.STUDENT;
+        for (Privilege enumPrivileges : enumAccess2.getPrivileges()) {
+            System.out.println(enumPrivileges.toString());
+        }
+        Access enumAccess3 = Access.MENTOR;
+        for (Privilege enumPrivileges : enumAccess3.getPrivileges()) {
+            System.out.println(enumPrivileges.toString());
+        }
     }
 }
