b72c243eca0af5e0e6c822263df7b96bc722cb71
rafalkordas <rafalkordas063@gmail.com>
2018-05-17 11:45:28 +0200

    Change readToFile and saveToFile

--- src/main/java/com/codecool/person/Student.java
+++ src/main/java/com/codecool/person/Student.java
@@ -1,4 +1,35 @@
 package com.codecool.person;
 
-public class Student {
+import com.codecool.details.Access;
+import com.codecool.details.Assignment;
+
+import java.util.List;
+
+public class Student extends CodecoolPerson {
+    private int attendance;
+    private List<Assignment> assignmentList;
+
+    public Student(String name, String surName, String login, String password, int attendance,
+                   List<Assignment> assignmentList) {
+        super(name, surName, login, password);
+        this.accessLevel = Access.STUDENT;
+        this.assignmentList = assignmentList;
+        this.attendance = attendance;
+    }
+
+    public int getAttendance() {
+        return this.attendance;
+    }
+
+    public List<Assignment> getAssignmentList() {
+        return assignmentList;
+    }
+
+    public void addAssignment(Assignment assignment) {
+        this.assignmentList.add(assignment);
+    }
+
+    public void setAttendance(int attend) {
+        this.attendance = attend;
+    }
 }
